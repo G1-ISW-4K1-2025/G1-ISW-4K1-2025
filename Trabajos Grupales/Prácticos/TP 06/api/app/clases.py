84ccd02e83b846bd270c8780996f80aece9d7a14
Camila Zamboni <96055@sistemas.frc.utn.edu.ar>
2025-10-17 20:06:23 -0300

    clase entrada y usuario

--- Trabajos Grupales/Prácticos/TP 06/api/app/clases.py
+++ Trabajos Grupales/Prácticos/TP 06/api/app/clases.py
@@ -4,3 +4,21 @@ class ValidacionError(Exception):
     """Excepción personalizada para errores de validación"""
     pass
 
+
+class Usuario:
+    def init(self, mail: str, contraseña: str):
+        self.mail = mail
+        self.contraseña = contraseña
+
+
+class Entrada:
+    def init(self, fecha_visita: date, edad_visitante: Optional[int], tipo_pase: str, precio: float):
+        if edad_visitante is None:
+            raise ValidacionError("La edad del visitante es requerida")
+        if edad_visitante < 0:
+            raise ValidacionError("La edad del visitante no puede ser negativa")
+
+        self.fecha_visita = fecha_visita
+        self.edad_visitante = edad_visitante
+        self.tipo_pase = tipo_pase
+        self.precio = precio
